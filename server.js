2c2d31bd804216aaacf7e72689909f93ad9985ca
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-11-12 14:31:28 +0500

    BD Routes

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const app = express();
 app.listen(3000 , ()=>{
     console.log("Server Started at port no. 3000");
 });
+
+app.get('/',(req,res)=>{
+    res.send("Hello, Kaise Ho Saare")
+})
+app.get('/login',(req,res)=>{
+    res.send("Login")
+})
+app.get('/singup',(req,res)=>{
+    res.send("Singup Page")
+})
+app.get('/blog',(req,res)=>{
+    res.send("Blog Post")
+})
